77b5d1e66a2a584709c61339b983945b34f7e697
urijtrufin <yura@gmail.com>
2025-05-17 14:14:05 +0300

    yura add migrations

--- backend_api/README.md
+++
@@ -1,2 +0,0 @@
-# Graduation-project-group3-
- This is a project for group 3.
--- backend_api/app/migrations/env.py
+++ backend_api/app/migrations/env.py
@@ -11,12 +11,12 @@ from settings import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
 config.set_section_option(
     config.config_ini_section, "sqlalchemy.url", f"{settings.DATABASE_URL_ASYNC}?async_fallback=True"
 )
 
 # Interpret the config file for Python logging.
-
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -71,7 +71,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
